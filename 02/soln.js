91eae2302ceee17725b9b0461f7b0a423d342e13
jmgisele <james.may.gisele@protonmail.com>
2022-02-12 17:41:32 -0500

    day 2 typescript

--- 02/soln.js
+++ 02/soln.js
@@ -1,23 +1,16 @@
-export { };
-const fs = require('fs')
-
-const data = fs.readFileSync('./data.txt', 'utf8')
-const result = data.split(/\r?\n/).map((a: string) => {
-    const arr = a.split(" ")
-    const ans = [arr[0], parseInt(arr[1])]
+"use strict";
+exports.__esModule = true;
+var fs = require('fs');
+var data = fs.readFileSync('./data.txt', 'utf8');
+var result = data.split(/\r?\n/).map(function (a) {
+    var arr = a.split(" ");
+    var ans = [arr[0], parseInt(arr[1])];
     return ans;
 });
-
-
 //pt 1
-interface Position {
-    horizontal: number
-    vertical: number
-}
-
-const coords: Position = { horizontal: 0, vertical: 0 }
-
-for (let entry of result) {
+var coords = { horizontal: 0, vertical: 0 };
+for (var _i = 0, result_1 = result; _i < result_1.length; _i++) {
+    var entry = result_1[_i];
     switch (entry[0]) {
         case 'forward':
             coords.horizontal += entry[1];
@@ -30,18 +23,16 @@ for (let entry of result) {
             break;
     }
 }
-
 console.log(coords.horizontal * coords.vertical);
-
 //pt 2
-let aim = 0;
-let newCoords: Position =  { horizontal: 0, vertical: 0 };
-
-for (let entry of result) {
+var aim = 0;
+var newCoords = { horizontal: 0, vertical: 0 };
+for (var _a = 0, result_2 = result; _a < result_2.length; _a++) {
+    var entry = result_2[_a];
     switch (entry[0]) {
         case 'forward':
             newCoords.horizontal += entry[1];
-            newCoords.vertical += aim * entry[1]
+            newCoords.vertical += aim * entry[1];
             break;
         case 'down':
             aim += entry[1];
@@ -51,5 +42,4 @@ for (let entry of result) {
             break;
     }
 }
-
 console.log(newCoords.horizontal * newCoords.vertical);
--- 02/soln.ts
+++ 02/soln.ts
@@ -1,15 +1,21 @@
-"use strict";
-exports.__esModule = true;
-var fs = require('fs');
-var data = fs.readFileSync('./data.txt', 'utf8');
-var result = data.split(/\r?\n/).map(function (a) {
-    var arr = a.split(" ");
-    var ans = [arr[0], parseInt(arr[1])];
+export { };
+const fs = require('fs')
+
+const data = fs.readFileSync('./data.txt', 'utf8')
+const result = data.split(/\r?\n/).map((a: string) => {
+    const arr = a.split(" ")
+    const ans = [arr[0], parseInt(arr[1])]
     return ans;
 });
-var coords = { horizontal: 0, vertical: 0 };
-for (var _i = 0, result_1 = result; _i < result_1.length; _i++) {
-    var entry = result_1[_i];
+interface Position {
+    horizontal: number
+    vertical: number
+}
+
+//pt 1
+const coords: Position = { horizontal: 0, vertical: 0 }
+
+for (let entry of result) {
     switch (entry[0]) {
         case 'forward':
             coords.horizontal += entry[1];
@@ -22,16 +28,18 @@ for (var _i = 0, result_1 = result; _i < result_1.length; _i++) {
             break;
     }
 }
+
 console.log(coords.horizontal * coords.vertical);
+
 //pt 2
-var aim = 0;
-var newCoords = { horizontal: 0, vertical: 0 };
-for (var _a = 0, result_2 = result; _a < result_2.length; _a++) {
-    var entry = result_2[_a];
+let aim = 0;
+const newCoords: Position =  { horizontal: 0, vertical: 0 };
+
+for (let entry of result) {
     switch (entry[0]) {
         case 'forward':
             newCoords.horizontal += entry[1];
-            newCoords.vertical += aim * entry[1];
+            newCoords.vertical += aim * entry[1]
             break;
         case 'down':
             aim += entry[1];
@@ -41,4 +49,5 @@ for (var _a = 0, result_2 = result; _a < result_2.length; _a++) {
             break;
     }
 }
+
 console.log(newCoords.horizontal * newCoords.vertical);
